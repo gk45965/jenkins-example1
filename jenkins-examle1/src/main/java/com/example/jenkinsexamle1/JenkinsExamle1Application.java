b9b22fa4fcbadbb2eef1f08c6be7ccdaa38df655
83Incs_11 <gaurav.kumar@83incs.com>
2019-12-30 11:38:19 +0530

    add jenkins

---
+++ jenkins-examle1/src/main/java/com/example/jenkinsexamle1/JenkinsExamle1Application.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsexamle1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsExamle1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsExamle1Application.class, args);
+	}
+
+}
---
+++ jenkins-examle1/src/test/java/com/example/jenkinsexamle1/JenkinsExamle1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsexamle1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsExamle1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

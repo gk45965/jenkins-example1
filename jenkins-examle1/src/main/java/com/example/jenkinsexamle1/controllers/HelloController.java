81b71d728368625ba945c14ea76f7abccfb64da7
83Incs_11 <gaurav.kumar@83incs.com>
2019-12-30 23:19:39 +0530

    update message 3

--- jenkins-examle1/src/main/java/com/example/jenkinsexamle1/controllers/HelloController.java
+++ jenkins-examle1/src/main/java/com/example/jenkinsexamle1/controllers/HelloController.java
@@ -1,16 +1,23 @@
 package com.example.jenkinsexamle1.controllers;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    private static final Logger LOG = LogManager.getLogger("org");
+
+
     @RequestMapping
     public String hello()
     {
-        return "hello jenkins example changes" ;
-     }
+        LOG.info("hello jenkins  api invoked successfully ");
+        System.out.println(" hello jenkins  api invoked successfully in system out");
+        return "hello jenkins example 2" ;
+    }
 
 
 }
